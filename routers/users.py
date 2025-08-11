6aa9d7172a5c2c0857e8bd58fa65109f2a654456
leesun04 <lth9913@naver.com>
2025-08-11 20:13:38 +0900

    로그인 기능 테스트 구현

--- routers/users.py
+++ routers/users.py
@@ -25,3 +25,20 @@ async def signup(user: UserIn):
     fake_db[user.username] = {"password":user.password}
     print(fake_db)
     return {"message": f"{user.username}님 회원가입 환영한다!!!!!"}
+
+@router.post("/login", response_model=UserOut, summary="로그인")
+async def login(user: UserIn):
+    if user.username not in fake_db:
+        print("User does not exist")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="존재하지 않는 사용자입니다.",
+        )
+    if fake_db[user.username]["password"] != user.password:
+        print("Incorrect password")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="비밀번호가 틀렸습니다.",
+        )
+    print(fake_db)
+    return {"username": user.username}
